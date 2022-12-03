69c9433fae3071b6b8a7fee85783d64bbe0c1559
Mariano Ibarra <marianoibarratesta@outlook.com>
2022-12-02 23:52:04 -0300

    models finished

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,27 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      default: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+    },
+
   });
 };
