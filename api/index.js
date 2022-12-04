d18ba4437af96179e5e9811fe44ba7032ef652ff
Mariano Ibarra <marianoibarratesta@outlook.com>
2022-12-03 23:25:56 -0300

    change on api/index.js

--- api/index.js
+++ api/index.js
@@ -19,8 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001);
+  server.listen(port, () => console.log(`Listening on port ${port}!`));
 });
