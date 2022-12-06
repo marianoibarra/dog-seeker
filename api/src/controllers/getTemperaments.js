309548d9536643d06348b802f08e481c249b5007
Mariano Ibarra <marianoibarratesta@outlook.com>
2022-12-06 19:18:37 -0300

    changes on api

--- api/src/controllers/getTemperaments.js
+++ api/src/controllers/getTemperaments.js
@@ -9,9 +9,8 @@ const getTemperaments = async () => {
     const temperaments = await axios(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
         .then(r => r.data)
         .then(dogs => {
-            let temperaments = dogs.filter(dog => dog.temperament !== undefined).map(dog => dog.temperament.split(', ')).flat().sort()
-            let set = new Set(temperaments)
-            return [...set]
+            let temperaments = dogs.filter(dog => dog.temperament !== undefined).map(dog => dog.temperament.split(', ')).flat().sort() 
+            return [...new Set(temperaments)]
         })
 
     for(let name of temperaments) {
