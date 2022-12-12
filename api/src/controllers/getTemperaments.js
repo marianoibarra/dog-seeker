430726c6f36b5956e8b4af63ca183ab0034830db
Mariano Ibarra <marianoibarratesta@outlook.com>
2022-12-12 02:11:21 -0300

    fix db getTemperaments

--- api/src/controllers/getTemperaments.js
+++ api/src/controllers/getTemperaments.js
@@ -4,26 +4,39 @@ const { Temperament } = require('../db')
 
 const getTemperaments = async () => {
 
-    let response = []
-
-    let sortFn = function(a, b) {
+    const sortFn = function(a, b) {
         let nameA = a.toLowerCase()
         let nameB = b.toLowerCase()
         if(nameA > nameB) return 1
         if(nameA < nameB) return -1
         return 0
     }
+    
+    const response = []
 
-    const temperaments = await axios(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
-        .then(r => r.data)
-        .then(dogs => {
-            let temperaments = dogs.filter(dog => dog.temperament !== undefined).map(dog => dog.temperament.split(', ')).flat().sort(sortFn) 
-            return [...new Set(temperaments)]
-        })
+    const temperamentFromDB = await Temperament.findAll()
 
-    for(let name of temperaments) {
-        let load = await Temperament.create({ name })
-        response.push(load)
+    if(temperamentFromDB.length > 0) {
+
+        const temperamentsFromAPI = await axios(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
+            .then(r => r.data)
+            .then(dogs => {
+                let temperaments = dogs
+                    .filter(dog => dog.temperament !== undefined)
+                    .map(dog => dog.temperament
+                    .split(', '))
+                    .flat()
+                    .sort(sortFn) 
+                return [...new Set(temperaments)]
+            })
+
+        for(let name of temperamentsFromAPI) {
+            let load = await Temperament.create({ name })
+            response.push(load)
+        }
+    
+    } else {
+        response = [...temperamentFromDB]
     }
 
     return response
