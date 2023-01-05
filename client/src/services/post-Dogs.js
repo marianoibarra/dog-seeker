3e8d5cbc742841be49071d009df9b4ca8c1833c4
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-01-05 14:23:45 -0300

    change on package json

--- client/src/services/post-Dogs.js
+++ client/src/services/post-Dogs.js
@@ -2,11 +2,7 @@ import axios from 'axios'
 import { BE_LINK } from './constants'
 
 const postDog_API = (data) => {
-    return axios({
-        method: 'POST',
-        url: `${BE_LINK}/dogs`,
-        data
-    })
+    return axios.post(`${BE_LINK}/dogs`, data)
 }
 
 export default postDog_API
