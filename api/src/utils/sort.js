4659ce7f7eacc0cf54be49f6b3c00179771d7647
Mariano Ibarra <marianoibarratesta@outlook.com>
2023-01-19 17:44:58 -0300

    fix on sortFn

--- api/src/utils/sort.js
+++ api/src/utils/sort.js
@@ -1,6 +1,6 @@
 const sortFn = function(a, b) {
-  let nameA = a.toLowerCase()
-  let nameB = b.toLowerCase()
+  let nameA = typeof a === 'object' ? a.name.toLowerCase() : a.toLowerCase()
+  let nameB = typeof b === 'object' ? b.name.toLowerCase() : b.toLowerCase()
   if(nameA > nameB) return 1
   if(nameA < nameB) return -1
   return 0
